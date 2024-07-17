b3f1402115f9d432de890f494974a6923de2bcf3
Alexandre Venito <avenito@yahoo.com.br>
2024-07-17 08:02:38 +0200

    READ_REGISTER working

--- src/modbus_c_plus_plus.cpp
+++ src/modbus_c_plus_plus.cpp
@@ -7,16 +7,19 @@
 //============================================================================
 
 #include <iostream>
+#include <thread>
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 #include "server/ModbusServer.h"
 
+#define	PORT	1501
+
 using namespace std;
 
 /* Instantiates the modbus server */
-ModbusServer	Server();
+ModbusServer	Server = ModbusServer();
 
 /* Declare the function to be called when pressing CTR+C to finish the program */
 void signal_callback_handler(int signum) {
@@ -25,12 +28,45 @@ void signal_callback_handler(int signum) {
 	   exit(EXIT_SUCCESS);
 }
 
+/* Example */
+void Example(){
+	while(true){
+		Server.registers[0]++;
+		this_thread::sleep_for(std::chrono::milliseconds(500));
+		Server.registers[1]--;
+	}
+}
+
+/* Modbus server */
+void MBServer(){
+	Server.runMbServer();
+}
+
 int main(int argc, char *argv[]) {
+
+	int port = PORT;
 
 	/* Declare the function to handler the CTR+C signal */
 	signal(SIGINT, signal_callback_handler);
 
-	int L_Port;
+	/* Initialize the modbus server. */
+	if (Server.init(port)){
+		cout << "Modbus server initialized ... listening port " << Server.getPort() << "." << endl;
+	} else {
+		cout << "Error!!!" << endl;
+		exit(0);
+	}
+
+	/* Start example thread. */
+	thread t1 (Example);
+	t1.detach();
+	/* Start modbus server.  */
+	thread t2 (MBServer);
+	t2.detach();
+
+	while(true){
+		this_thread::sleep_for(std::chrono::milliseconds(1500));
+	}
 
 	return 0;
 }
