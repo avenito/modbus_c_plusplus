1ff0410323bf5fafe8dce2f50240de11c51f2a58
Alexandre Venito <avenito@yahoo.com.br>
2024-09-03 08:25:48 +0200

    Wip

--- src/modbus_c_plus_plus.cpp
+++ src/modbus_c_plus_plus.cpp
@@ -1,9 +1,10 @@
 //============================================================================
 // Name        : modbus_c_plus_plus.cpp
 // Author      : Alexandre Venito
-// Version     : 1.0.0
-// Copyright   : 
+// Version     : 1.0.1
+// Copyright   : MiT
 // Description : Example how to use Modbus Server C++
+// 				 Exemplo de como usar o Modbus Server C++
 //============================================================================
 
 #include <iostream>
@@ -14,20 +15,24 @@
 
 #include "server/ModbusServer.h"
 
-#define	PORT		1501
-#define VAZAO_CX01	5
-#define VAZAO_VL01	5
-#define VAZAO_VL02	5
-#define VAZAO_VL03	5
-#define VAZAO_VL04	5
+#define	PORT			1501
+#define FLOWRATE_TK01	5
+#define FLOWRATE_VL01	5
+#define FLOWRATE_VL02	5
+#define FLOWRATE_VL03	5
+#define FLOWRATE_VL04	5
 
 using namespace std;
 
 /* Instantiates the modbus server */
+/* Instancia o modbus server */
+
 ModbusServer	Server = ModbusServer();
-int	VzV01, VzV02;
+int				FRV01, FRV02;
 
 /* Declare the function to be called when pressing CTR+C to finish the program */
+/* Declara a função para ser chamda quando pressionarmos CTR+C para terminar o programa */
+
 void signal_callback_handler(int signum) {
 	   cout << "\nExample finished! " << endl;
 	   // Terminate program
@@ -39,18 +44,18 @@ void signal_callback_handler(int signum) {
  *
  * Inputs:
  *
- * 0 - Nivel Caixa 01
- * 1 - Nivel Caixa 02
- * 2 - Nivel Caixa 03
- * 3 - RPM Bomba 01
- * 4 - Valvula 01
- * 5 - Valvula 02
- * 6 - Valvula 03
+ * 0 - Level Tank 01 / Nível Caixa 01
+ * 1 - Level Tank 02 / Nível Caixa 02
+ * 2 - Level Tank 03 / Nível Caixa 03
+ * 3 - RPM Pump 01 / RPM Bomba 01
+ * 4 - Valve 01 / Válvula 01
+ * 5 - Valve 02 / Válvula 02
+ * 6 - Valve 03 / Válvula 03
  */
 
-#define Nivel_Cx01		Server.inputs[0]
-#define Nivel_Cx02		Server.inputs[1]
-#define Nivel_Cx03		Server.inputs[2]
+#define Level_Tk01		Server.inputs[0]
+#define Level_Tk02		Server.inputs[1]
+#define Level_Tk03		Server.inputs[2]
 #define RPM_FB			Server.inputs[3]
 #define Valv01_FB		Server.inputs[4]
 #define Valv02_FB		Server.inputs[5]
@@ -58,26 +63,26 @@ void signal_callback_handler(int signum) {
 
 /* Registers:
  *
- * 0 - Ajuste RPM Bomba 01
- * 1 - Ajuste valvula 01
- * 2 - Ajuste valvula 02
- * 3 - Ajuste valvula 03
+ * 0 - Setpoint RPM Pump 01 / Ajuste RPM Bomba 01
+ * 1 - Setpoint valve 01 / Ajuste valvula 01
+ * 2 - Setpoint valve 02 / Ajuste valvula 02
+ * 3 - Setpoint valve 03 / Ajuste valvula 03
  */
 
-#define Ajuste_RPM		Server.registers[0]
-#define Ajuste_Valv01	Server.registers[1]
-#define Ajuste_Valv02	Server.registers[2]
-#define Ajuste_Valv03	Server.registers[3]
+#define Setpoint_RPM		Server.registers[0]
+#define Setpoint_Valv01		Server.registers[1]
+#define Setpoint_Valv02		Server.registers[2]
+#define Setpoint_Valv03		Server.registers[3]
 
 /* Dicrete-Inputs:
  *
- * 0 - Bomba ligada
- * 1 - Caixa 01 transbordando
+ * 0 - Pump 01 ON / Bomba 01 ON
+ * 1 - Tank 01 overflowing / Caixa 01 transbordando
  * 2 - Caixa 02 transbordando
  * 3 - Caixa 03 transbordando
- * 4 - Caixa 01 baixo nivel
- * 5 - Caixa 02 baixo nivel
- * 6 - Caixa 03 baixo nivel
+ * 4 - Caixa 01 nível baixo
+ * 5 - Caixa 02 nível baixo
+ * 6 - Caixa 03 nível baixo
  */
 
 #define Bomba_Ligada	Server.discrete_inputs[0]
@@ -110,7 +115,7 @@ void Example(){
 	while(true){
 
 		/* Bomba 01 - Bombeia para Cx01 */
-		Bomba_Ligada = Server.coils[0];
+		Bomba_Ligada = Liga_Bomba;
 
 		if (Bomba_Ligada) {
 			if (RPM_FB >= 100){
@@ -128,57 +133,57 @@ void Example(){
 		}
 
 		/* Caixa 01 */
-		if (Server.discrete_inputs[0] && (RPM_FB > 0)){
-			Nivel_Cx01 += VAZAO_CX01;
+		if (Bomba_Ligada && (RPM_FB > 0)){
+			Level_Tk01 += FLOWRATE_TK01;
 		}
-		if (Nivel_Cx01 >= 100){
+		if (Level_Tk01 >= 100){
 			Cx01_Transb = 1;
 		} else {
 			Cx01_Transb = 0;
 		}
 
 		/* Valvula 01 ajustavel - Liga Cx01 - Cx02 */
-		Valv01_FB = Ajuste_Valv01;
-		if (Valv01_FB > 0 && Nivel_Cx01 > 0){
-			VzV01 = (VAZAO_VL01 * Valv01_FB / 100);
-			Nivel_Cx01 -= VzV01; // esvazia Cx01
-			Nivel_Cx02 += VzV01; // enche Cx02
+		Valv01_FB = Setpoint_Valv01;
+		if (Valv01_FB > 0 && Level_Tk01 > 0){
+			FRV01 = (FLOWRATE_VL01 * Valv01_FB / 100);
+			Level_Tk01 -= FRV01; // esvazia Cx01
+			Level_Tk02 += FRV01; // enche Cx02
 		}
 		/* Valvula 02 ajustavel - Liga Cx01 - Cx03 */
-		Valv02_FB = Ajuste_Valv02;
-		if (Valv02_FB > 0 && Nivel_Cx01 > 0){
-			VzV02 = (VAZAO_VL02 * Valv02_FB / 100);
-			Nivel_Cx01 -= VzV02; // esvazia Cx01
-			Nivel_Cx03 += VzV02; // enche Cx03
+		Valv02_FB = Setpoint_Valv02;
+		if (Valv02_FB > 0 && Level_Tk01 > 0){
+			FRV02 = (FLOWRATE_VL02 * Valv02_FB / 100);
+			Level_Tk01 -= FRV02; // esvazia Cx01
+			Level_Tk03 += FRV02; // enche Cx03
 		}
 
 		/* Valvula 03 on/off - Saida Cx02 */
-		Server.discrete_inputs[4] = Server.coil[1];
-		if (Server.discrete_input[4] && Nivel_Cx02 > 0){
-			Nivel_Cx02 -= VAZAO_VL03;
+		Cx01_Baixo = Server.coils[1];
+		if (Cx01_Baixo && Level_Tk02 > 0){
+			Level_Tk02 -= FLOWRATE_VL03;
 		}
-		if (Nivel_Cx02 >= 100){
-			Server.discrete_inputs[2] = 1;
+		if (Level_Tk02 >= 100){
+			Cx02_Transb = 1;
 		} else {
-			Server.discrete_inputs[2] = 0;
+			Cx02_Transb = 0;
 		}
 
 
 		/* Valvula 04 on/off - Saida Cx03 */
-		Server.discrete_inputs[5] = Server.coil[2];
-		if (Server.discrete_inputs[5] && Nivel_Cx03 > 0){
-			Nivel_Cx03 -= VAZAO_VL04;
+		Server.discrete_inputs[5] = Server.coils[2];
+		if (Server.discrete_inputs[5] && Level_Tk03 > 0){
+			Level_Tk03 -= FLOWRATE_VL04;
 		}
-		if (Nivel_Cx03 >= 100){
-			Server.discrete_inputs[3] = 1;
+		if (Level_Tk03 >= 100){
+			Cx03_Transb = 1;
 		} else {
-			Server.discrete_inputs[3] = 0;
+			Cx03_Transb = 0;
 		}
 
 		/* Check limites */
-		Nivel_Cx01 = CheckLimit(Nivel_Cx01, 100, 0);
-		Nivel_Cx02 = CheckLimit(Nivel_Cx02, 100, 0);
-		Nivel_Cx03 = CheckLimit(Nivel_Cx03, 100, 0);
+		Level_Tk01 = CheckLimit(Level_Tk01, 100, 0);
+		Level_Tk02 = CheckLimit(Level_Tk02, 100, 0);
+		Level_Tk03 = CheckLimit(Level_Tk03, 100, 0);
 		RPM_FB = CheckLimit(RPM_FB, 100, 0);
 		Valv01_FB = CheckLimit(Valv01_FB, 100, 0);
 		Valv02_FB = CheckLimit(Valv02_FB, 100, 0);
